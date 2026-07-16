f1b37da33e05a78c0d3617d8494494eea9711012
Afton Gauntlett <afton.gauntlett@gmail.com>
2026-07-16 16:14:20 -0400

    updating favicon and meta data

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://queen-city-magic.vercel.app",
   integrations: [react(), sitemap()],
   legacy: {
     collectionsBackwardsCompat: true,
